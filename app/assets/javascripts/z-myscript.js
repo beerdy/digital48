1e51ab1e5a109a45ebe6d28e91c2e2577dfcd5df
Alexander <mr.beerdy@gmail.com>
2017-08-29 15:10:23 +0300

    replace preloader

--- app/assets/javascripts/z-myscript.js
+++ app/assets/javascripts/z-myscript.js
@@ -5,12 +5,23 @@ $(function() {
   // END MainMenu  
 });
 
+jQuery(document).ready(function($) {  
+  // site preloader -- also uncomment the div in the header and the css style for #preloader
+  $(window).load(function(){
+      setTimeout(function(){
+          $("#timer51029eaee7c27d9df0fd78bb15c7eb75").css("min-width","50px");
+          $("#timer51029eaee7c27d9df0fd78bb15c7eb75").css("position","relative");
+      }, 1000);
+    $('#preloader').fadeOut('slow',function(){$(this).remove();});
+  });
+});
+
 // init
   var preTimeSleep = 1000;
 
   jQuery(document).on("turbolinks:load", function() {
 
-    // Start Preloader
+    /* Start Preloader
     var hellopreloader = document.getElementById("hellopreloader_preload");
     function fadeOutnojquery(el){
       el.style.opacity = 1;
@@ -27,7 +38,7 @@ $(function() {
       setTimeout(function(){ fadeOutnojquery(hellopreloader);},preTimeSleep);
       preTimeSleep = 1;
     }
-    // END Preloader
+    */// END Preloader
 
 
 
