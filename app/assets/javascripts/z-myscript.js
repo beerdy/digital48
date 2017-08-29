7afd3664e8f6d3adaa5bef91ad3f295203d2ee47
Alexander <mr.beerdy@gmail.com>
2017-08-29 16:21:49 +0300

    check turbolinks

--- app/assets/javascripts/z-myscript.js
+++ app/assets/javascripts/z-myscript.js
@@ -1,9 +1,7 @@
-// init main menu
-$(function() {
-  // MainMenu
-  cbpHorizontalMenu.init();
-  // END MainMenu  
-});
+function goTo( link ){
+  window.location = link;
+  return false;
+}
 
 jQuery(document).ready(function($) {  
   // site preloader -- also uncomment the div in the header and the css style for #preloader
@@ -21,29 +19,19 @@ jQuery(document).ready(function($) {
 
   jQuery(document).on("turbolinks:load", function() {
 
-
-
     // Start FLEXSlider
     jQuery('.flexslider').flexslider({
       animation: "slide"
     });
     // END FLEXSlider
 
+    //preloader
 
-
-    /* RedConnect
-    var imported = document.createElement('script');
-    imported.src = 'https://web.redhelper.ru/service/main.js?c=digital48';
-    imported.charset = "utf-8";
-    imported.id="rhlpscrtg";
-    imported.async = "async";
-    document.head.appendChild(imported);
-    */// END RedConnect
-
-    jQuery("#indexpage").on("click", function() {
-      window.location = '/company/index'
-    });
-
+      setTimeout(function(){
+          $("#timer51029eaee7c27d9df0fd78bb15c7eb75").css("min-width","50px");
+          $("#timer51029eaee7c27d9df0fd78bb15c7eb75").css("position","relative");
+      }, 1000);
+      jQuery('#preloader').fadeOut('slow',function(){$(this).remove();});
   });
 
 
